f0387d5e34875fb7aa0b9d6f2032da8fa877f6d2
Myho <gabriel.haeri@gmail.com>
2015-10-14 01:04:12 -0300

    hue

---
+++ api-java-time/src/test/java/br/com/huegroup/api/date/TesteTest.java
@@ -0,0 +1,14 @@
+package br.com.huegroup.api.date;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TesteTest {
+
+    @Test
+    public void hue() {
+        double arara = 1;
+        assertEquals(3, arara, 0.0);
+    }
+}
